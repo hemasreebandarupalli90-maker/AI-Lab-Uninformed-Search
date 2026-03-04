1d2a41754b7aa9c1765023d8a850b43e7d437a26
hemasreebandarupalli90-maker <hemasreebandarupalli90@gmail.com>
2026-03-04 21:04:01 +0530

    Added BFS

--- TuringTest_Captcha/search_algorithms/bfs.py
+++ TuringTest_Captcha/search_algorithms/bfs.py
@@ -1,1 +1,19 @@
+from collections import deque
 
+def bfs(problem):
+    frontier = deque([problem.initial_state])
+    explored = set()
+
+    while frontier:
+        state = frontier.popleft()
+
+        if problem.goal_test(state):
+            return state
+
+        explored.add(state)
+
+        for child in problem.successors(state):
+            if child not in explored:
+                frontier.append(child)
+
+    return None
