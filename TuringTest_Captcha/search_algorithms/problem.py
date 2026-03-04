56972bf711bf6123e16b592f3831f7812000e9a5
hemasreebandarupalli90-maker <hemasreebandarupalli90@gmail.com>
2026-03-04 21:04:32 +0530

    Added Problem base class

--- TuringTest_Captcha/search_algorithms/problem.py
+++ TuringTest_Captcha/search_algorithms/problem.py
@@ -1,1 +1,9 @@
+class Problem:
+    def __init__(self, initial_state):
+        self.initial_state = initial_state
 
+    def goal_test(self, state):
+        pass
+
+    def successors(self, state):
+        pass
