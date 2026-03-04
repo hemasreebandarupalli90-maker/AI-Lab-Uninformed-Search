3b3bc7d5112f659852f80c9ad343c114d1af3e9a
hemasreebandarupalli90-maker <hemasreebandarupalli90@gmail.com>
2026-03-04 21:03:35 +0530

    Added DFS
    Implement depth-first search algorithm for problem solving.

--- TuringTest_Captcha/search_algorithms/dfs.py
+++ TuringTest_Captcha/search_algorithms/dfs.py
@@ -1,1 +1,17 @@
+def dfs(problem):
+    frontier = [problem.initial_state]
+    explored = set()
 
+    while frontier:
+        state = frontier.pop()
+
+        if problem.goal_test(state):
+            return state
+
+        explored.add(state)
+
+        for child in problem.successors(state):
+            if child not in explored:
+                frontier.append(child)
+
+    return None
